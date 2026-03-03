ee56939d5c9f25268f86a4dc9b359e501dd32708
gamsistemasinteligentes-png <gamsistemasinteligentes@gmail.com>
2026-03-03 07:59:42 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,26 @@
-const CACHE_NAME = 'secu-v3';
+const CACHE_NAME = 'secu-pwa-v4';
 const assets = [
   './',
   './index.html'
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
-  self.skipWaiting(); // Fuerza la actualización inmediata
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+  )));
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
